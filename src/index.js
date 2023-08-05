29bed62c236b0bd88fea595f70bea9b8e6d3c5ab
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2023-08-05 01:35:11 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { ByteArray } from "./jsvm/bytearray.js";
-import { toByteCode } from "./jsvm/bytecode.js";
-import { runByteCode } from "./jsvm/runtime.js";
-
-const bytecode = toByteCode(`
-console.log("Hello world");
-`);
-
-runByteCode(bytecode.bytes);
